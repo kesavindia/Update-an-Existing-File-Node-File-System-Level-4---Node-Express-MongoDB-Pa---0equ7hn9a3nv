78e77c2a21c1ee3a6f5aef367bc6f1a086ee122c
kesavindia <kesavindia83@gmail.com>
2023-07-15 17:18:43 +0530

    done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ const fileName = "myfile.txt";
 const updateFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+  try{
+    const existingContent= await fs.readFile(fileName,"utf-8")
+    const updatedContent= existingContent+fileContent;
+    await fs.writeFile(fileName,updatedContent);
+    return updatedContent;
+  }
+catch(error){
+    console.error("Error updating file:",error);
+    throw error;
+}
   
 };
 
